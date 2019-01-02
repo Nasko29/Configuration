c0813948be72cd03fa3c7d7498a964206f6a7e05
Nasko Grozdanov <Nasko@Ludmilas-MBP.lan>
2019-01-02 12:48:07 +0000

    modified boostrap_ym, working on aws

--- bootstrap_yum.sh
+++ bootstrap_yum.sh
@@ -26,12 +26,12 @@ mv bashrc.txt .bashrc
 source .bashrc
 
 # fetch application preferences
-wget -q https://raw.githubusercontent.com/Nasko29/Configuration/master/tmux.conf.txt -O .tmux.conf
-wget -q https://raw.githubusercontent.com/Nasko29/Configuration/master/vimrc.txt -P .vim -O .vimrc
+wget  -q -O .tmux.conf https://raw.githubusercontent.com/Nasko29/Configuration/master/tmux.conf.txt
+wget  -q -P .vim -O .vimrc https://raw.githubusercontent.com/Nasko29/Configuration/master/vimrc.txt
 mkdir -p .vim
 mkdir -p .vim/colors
-wget -q https://raw.githubusercontent.com/agude/vim-eldar/master/colors/eldar.vim -O .vim/colors/eldar.vim
+wget -q -O .vim/colors/eldar.vim https://raw.githubusercontent.com/agude/vim-eldar/master/colors/eldar.vim
 
 # python installations
-pip3 -q install virtualenv --user
-pip3 -q install zappa FLask progress boto3 feedparser newspaper3k --user
+sudo pip3 -q install virtualenv --user
+sudo pip3 -q install zappa FLask progress boto3 feedparser newspaper3k --user
