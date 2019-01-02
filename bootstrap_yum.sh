72454af3249520fb6cce5d8720638bca6f4c2250
Nasko Grozdanov <Nasko@Ludmilas-MBP.lan>
2019-01-02 12:37:49 +0000

    modified bootstrap_yum

--- bootstrap_yum.sh
+++ bootstrap_yum.sh
@@ -18,13 +18,20 @@ sudo yum -q -y install awscli
 
 # alias python
 alias python=python3
+alias pip=pip3
 
 # fetch bash prefrences
-wget https://raw.githubusercontent.com/Nasko29/Configuration/master/bashrc.txt
-rm .bashrc ; mv bashrc.txt .bashrc
+wget -q https://raw.githubusercontent.com/Nasko29/Configuration/master/bashrc.txt
+mv bashrc.txt .bashrc
 source .bashrc
 
 # fetch application preferences
-wget https://raw.githubusercontent.com/Nasko29/Configuration/master/.tmux.conf
-wget https://raw.githubusercontent.com/Nasko29/Configuration/master/.vimrc -P .vim
+wget -q https://raw.githubusercontent.com/Nasko29/Configuration/master/tmux.conf.txt -O .tmux.conf
+wget -q https://raw.githubusercontent.com/Nasko29/Configuration/master/vimrc.txt -P .vim -O .vimrc
 mkdir -p .vim
+mkdir -p .vim/colors
+wget -q https://raw.githubusercontent.com/agude/vim-eldar/master/colors/eldar.vim -O .vim/colors/eldar.vim
+
+# python installations
+pip3 -q install virtualenv --user
+pip3 -q install zappa FLask progress boto3 feedparser newspaper3k --user
